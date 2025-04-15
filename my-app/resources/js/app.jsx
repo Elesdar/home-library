44dd48cedb33bea704268a0058d08bb7a96a29bc
Vladislava Mishina <vlada.green67@gmail.com>
2025-04-15 19:07:44 +0400

    Установка React + Sanctum

---
+++ my-app/resources/js/app.jsx
@@ -0,0 +1,7 @@
+import './bootstrap';
+import React from 'react';
+import "../css/app.css";
+import ReactDOM from 'react-dom/client';
+import ExampleComponent from './components/ExampleComponent.jsx';
+
+ReactDOM.createRoot(document.getElementById("example-component")).render(<ExampleComponent />);
